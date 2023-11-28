9694ee3eb38f65c4b309556cecffb389e10e0d5a
Vlad Cervac <cervacvad32@gmail.com>
2023-11-28 10:18:39 +0100

    add dependencies for app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { EmployeeService } from './employee/employee.service';
 import { EmployeeController } from './employee/employee.controller';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 
 @Module({
-  imports: [],
+  imports: [
+	PrismaModule,
+	ConfigModule.forRoot({
+		isGlobal: true
+	})
+],
   providers: [EmployeeService],
   controllers: [EmployeeController],
 
