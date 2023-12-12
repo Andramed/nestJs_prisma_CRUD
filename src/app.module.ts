e1c1ecb663ed252fbe38cf904449d09737abf486
Vlad Cervac <cervacvad32@gmail.com>
2023-12-12 10:44:32 +0100

    other changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,33 @@ import { EmployeeService } from './employee/employee.service';
 import { EmployeeController } from './employee/employee.controller';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+
+import { SignupController } from './auth/signup/signup.controller';
+import { SignupService } from './auth/signup/signup.service';
+import { JwtService } from '@nestjs/jwt';
 
 
 
 @Module({
   imports: [
-	PrismaModule,
-	ConfigModule.forRoot({
-		isGlobal: true
-	})
-],
-  providers: [EmployeeService],
-  controllers: [EmployeeController],
+		PrismaModule,
+		ConfigModule.forRoot({
+			isGlobal: true
+		}),
+	],
+  providers: [
+		EmployeeService, 
+		AuthService, 
+		SignupService,
+		JwtService
+	],
+  controllers: [
+		EmployeeController, 
+		AuthController, 
+		SignupController
+	],
 
 })
 export class AppModule {}
