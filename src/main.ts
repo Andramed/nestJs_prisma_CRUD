28cd9ae9ad3a48f3319c644b4c72888b8dfdee99
Vlad Cervac <cervacvad32@gmail.com>
2023-12-20 11:10:54 +0100

    add cors options

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,35 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AddManagerService } from './auth/add-manager/add-manager.service';
 import { ConfigService } from '@nestjs/config';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+const corsOption: CorsOptions = {
+	
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const userService = app.get(AddManagerService);
   const config = app.get(ConfigService)
-  userService.creteAdminUser(
+  const createdAdminUSer = await userService.creteAdminUser(
 	{
 		email: config.get("ADMIN_LOGIN"),
 		password: config.get("ADMIN_PASSWORD")
 	}
   )
 
-  app.enableCors()
+  app.enableCors(
+	corsOption
+  )
   await app.listen(3000);
+
+  console.log({
+	status: 'start to port 3000',
+	user: createdAdminUSer
+
+  });
+  
 }
 bootstrap();
